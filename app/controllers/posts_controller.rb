34aa34f9bc95af60cb801ad0cb9c624240a174cd
Matt Rice <mattrice12@outlook.com>
2016-07-04 19:35:02 -0500

    Tried hard at getting only first 300 characters of each post to show up. Couldn't do it.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,12 +6,7 @@ class PostsController < ApplicationController
     else
       render App.posts[0..9].to_json, status: "200 OK"
     end
-
-
-
-    # pp = App.posts.map { |a| a.to_json[0..300] }
-    # render pp.to_json, status: "200 OK"
-
+    
     # For some reason, the parser is storing "page" as a string rather than a symbol. I compared this with Wednesday's assignment, which stored "page" as a symbol. This weekend's parser says it stores "page" as a symbol => params.store(k.to_sym, v)... however, it's not doing this at all. Substituting in Wednesday's parser does not change the result. I really can't figure out why it's storing "page" as a string rather than a symbol... so this is why my index looks the way it does.
     # render params.values[0].to_json, status: "200 OK"
     # to test: puts @request[:params]
